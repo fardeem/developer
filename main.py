b7530303fd961885378c1852033950f19437f55d
Fardeem Munir <munirfardeem@gmail.com>
2023-05-15 17:40:03 -0500

    Fix concurrency issue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import ast
 import os
-import sys
 
 import modal
 
@@ -19,8 +18,7 @@ openai_model_max_tokens = 2000  # i wonder how to tweak this properly
         backoff_coefficient=2.0,
         initial_delay=1.0,
     ),
-    # concurrency_limit=5,
-    # timeout=120,
+    concurrency_limit=5,
 )
 def generate_response(system_prompt, user_prompt, *args):
     import openai
